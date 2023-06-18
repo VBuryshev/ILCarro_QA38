cca4424803aeb6a828e3fe3b025d480895f7b9cb
VBuryshev <v.buryshev@gmail.com>
2023-06-18 21:45:44 +0300

    HW_07
    Assert is not working =(

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,11 +1,38 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 
 public class HelperUser extends HelperBase
 {
     public HelperUser(WebDriver wd)
     {
         super(wd);
+    }
+    public boolean isLogged()
+    {
+        return isElementPresent(By.cssSelector("body > app-root > app-navigator > div.header > a:nth-child(5)"));
+    }
+
+    public void logout()
+    {
+        click(By.xpath("/html/body/app-root/app-navigator/div[1]/a[5]"));
+    }
+
+    public void openLoginForm()
+    {
+        wd.findElement(By.xpath("/html/body/app-root/app-navigator/div[1]/a[6]")).click();
+    }
+
+    public void fillLoginForm(String email, String password)
+    {
+        type(By.xpath("//*[@id=\"email\"]"), email);
+        type(By.xpath("//*[@id=\"password\"]"), password);
+    }
+
+    public void submitLogin()
+    {
+        click(By.xpath("/html/body/app-root/app-navigator/app-login-registration/div/div/div/app-login/form/button"));
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,10 +1,30 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase
 {
-    @Test
-    public void loginPositive()
+    //HW_07_Task_1
+    @BeforeMethod
+    public void Preconditions()
     {
-        
+        if(app.getUser().isLogged())
+        {
+            app.getUser().logout();
+        }
+
+    }
+    @Test
+    public void loginPositiveTestBase()
+    {
+        String email = "vasya@mail.com", password = "Act123456$";
+        app.getUser().openLoginForm();
+        app.getUser().fillLoginForm(email, password);
+        app.getUser().submitLogin();
+        app.getUser().pause(5000);
+        //Assert.assertTrue(app.getUser().isElementPresent(By.linkText("Logged in success")));
+        //Assert.assertTrue(app.getUser().isElementPresent(By.xpath("h1[class='title']")));
+        //Assert.assertTrue(app.getUser().isElementPresent(By.cssSelector("#mat-dialog-1 > app-error > div > div > button")));
     }
 }
