c51277eec14303724e089982769c6f958f0c88c8
VBuryshev <v.buryshev@gmail.com>
2023-06-20 23:44:33 +0300

    CW_07

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,5 +1,6 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
@@ -22,7 +23,7 @@ public class HelperUser extends HelperBase
 
     public void openLoginForm()
     {
-        wd.findElement(By.xpath("/html/body/app-root/app-navigator/div[1]/a[6]")).click();
+        wd.findElement(By.xpath("//*[.=' Log in ']")).click();
     }
 
     public void fillLoginForm(String email, String password)
@@ -30,9 +31,14 @@ public class HelperUser extends HelperBase
         type(By.xpath("//*[@id=\"email\"]"), email);
         type(By.xpath("//*[@id=\"password\"]"), password);
     }
+    public void fillLoginForm(User user)
+    {
+        type(By.xpath("//*[@id=\"email\"]"), user.getEmail());
+        type(By.xpath("//*[@id=\"password\"]"), user.getPassword());
+    }
 
     public void submitLogin()
     {
-        click(By.xpath("/html/body/app-root/app-navigator/app-login-registration/div/div/div/app-login/form/button"));
+        wd.findElement(By.xpath("//button[@type='submit']")).submit();
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,7 @@
+import models.User;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -7,7 +9,7 @@ public class LoginTests extends TestBase
 {
     //HW_07_Task_1
     @BeforeMethod
-    public void Preconditions()
+    public void preconditions()
     {
         if(app.getUser().isLogged())
         {
@@ -15,16 +17,35 @@ public class LoginTests extends TestBase
         }
 
     }
+//    @Test
+//    public void loginPositive()
+//    {
+//        String email = "vasya@mail.com", password = "Act123456$";
+//        app.getUser().openLoginForm();
+//        app.getUser().fillLoginForm(email, password);
+//        app.getUser().submitLogin();
+//        app.getUser().pause(5000);
+//        //Assert.assertTrue(app.getUser().isElementPresent(By.linkText("Logged in success")));
+//        //Assert.assertTrue(app.getUser().isElementPresent(By.xpath("h1[class='title']")));
+//        //Assert.assertTrue(app.getUser().isElementPresent(By.cssSelector("#mat-dialog-1 > app-error > div > div > button")));
+//        app.getUser().logout();
+//    }
     @Test
-    public void loginPositive()
+    public void loginPositiveUserData()
     {
-        String email = "vasya@mail.com", password = "Act123456$";
+        User user = new User()
+                .withEmail("vasya@mail.com")
+                .withPassword("Act123456");
         app.getUser().openLoginForm();
-        app.getUser().fillLoginForm(email, password);
+        app.getUser().pause(2000);
+        app.getUser().fillLoginForm(user);
         app.getUser().submitLogin();
-        app.getUser().pause(5000);
-        //Assert.assertTrue(app.getUser().isElementPresent(By.linkText("Logged in success")));
-        //Assert.assertTrue(app.getUser().isElementPresent(By.xpath("h1[class='title']")));
-        //Assert.assertTrue(app.getUser().isElementPresent(By.cssSelector("#mat-dialog-1 > app-error > div > div > button")));
     }
+
+    @AfterMethod
+    public void postconditions()
+    {
+
+    }
+
 }
